75008d2b15d5d9c7a8669c120751ebb6ff4dc563
Crystal Jade Allen-Washington <crystaljade@macbooks-air>
2017-06-28 20:46:07 -0400

    Initial Commit

---
+++ GoodNewsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodNewsApp
+//
+//  Created by Crystal Jade Allen-Washington on 6/28/17.
+//  Copyright © 2017 Crystal Jade Allen-Washington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
