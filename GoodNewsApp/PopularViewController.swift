0703da08aa48de9a9f2a65898a6a187cae0c5f6c
Crystal Jade Allen-Washington <crystaljade@macbooks-air.attlocal.net>
2017-06-29 01:52:15 -0400

    first commit

---
+++ GoodNewsApp/PopularViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PopularViewController.swift
+//  GoodNewsApp
+//
+//  Created by Crystal Jade Allen-Washington on 6/28/17.
+//  Copyright © 2017 Crystal Jade Allen-Washington. All rights reserved.
+//
+
+import Foundation
---
+++ GoodNewsApp/TodayViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TodayViewController.swift
+//  GoodNewsApp
+//
+//  Created by Crystal Jade Allen-Washington on 6/29/17.
+//  Copyright © 2017 Crystal Jade Allen-Washington. All rights reserved.
+//
+
+import Foundation
